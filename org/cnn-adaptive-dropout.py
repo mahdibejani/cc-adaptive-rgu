ab529a2fda01c61339d94c140682331cc557e8e2
Admin <mohammad.mahdi.bejani@gmail.com>
2018-02-25 10:58:23 -0800

    update

--- org/cnn-adaptive-dropout.py
+++ org/cnn-adaptive-dropout.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from svhn import SVHN
 import matplotlib.pyplot as plt
 from random import randint
+import numpy as np
 
 # Parameters
 learning_rate = 1e-3
@@ -13,7 +14,7 @@ display_step = 100
 channels = 3
 image_size = 32
 n_classes = 10
-dropout = 1.0
+keep_prob1_value, keep_prob2_value = 1.0, 1.0
 hidden_1 = 256
 hidden_2 = 128
 depth_1 = 16
@@ -76,40 +77,39 @@ def normalize(x):
 
 
 def cnn(x):
-    # Batch normalization
     x = normalize(x)
 
-    # Convolution 1 -> RELU -> Max Pool
     convolution1 = convolution(x, weights["layer1"])
     relu1 = tf.nn.relu(convolution1 + biases["layer1"])
     maxpool1 = max_pool(relu1)
 
-    # Convolution 2 -> RELU -> Max Pool
     convolution2 = convolution(maxpool1, weights["layer2"])
     relu2 = tf.nn.relu(convolution2 + biases["layer2"])
     maxpool2 = max_pool(relu2)
 
-    # Convolution 3 -> RELU -> Max Pool
     convolution3 = convolution(maxpool2, weights["layer3"])
     relu3 = tf.nn.relu(convolution3 + biases["layer3"])
     maxpool3 = max_pool(relu3)
 
-    # Fully Connected Layer
     shape = maxpool3.get_shape().as_list()
     reshape = tf.reshape(maxpool3, [-1, shape[1] * shape[2] * shape[3]])
     fc4 = tf.nn.relu(tf.matmul(reshape, weights["layer4"]) + biases["layer4"])
 
-    # Dropout Layer
-    keep_prob_constant = tf.placeholder(tf.float32)
-    fc4_dropout = tf.nn.dropout(fc4, keep_prob_constant)
+    keep_prob1 = tf.placeholder(tf.float32)
+    fc4_dropout = tf.nn.dropout(fc4, keep_prob1)
 
     fc5 = tf.nn.relu(tf.matmul(fc4_dropout, weights["layer5"]) + biases["layer5"])
 
-    return tf.matmul(fc5, weights["layer6"]) + biases["layer6"], keep_prob_constant
+    keep_prob2 = tf.placeholder(tf.float32)
+    fc5_dropout = tf.nn.dropout(fc5, keep_prob2)
+
+    y_conv = tf.matmul(fc5_dropout, weights["layer6"]) + biases["layer6"]
+
+    return y_conv, keep_prob1, keep_prob2
 
 
 # Build the graph for the deep net
-y_conv, keep_prob = cnn(X)
+y_conv, keep_prob1, keep_prob2 = cnn(X)
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=y_conv))
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
@@ -129,55 +129,92 @@ with tf.Session() as sess:
     train_losses = []
     test_accuracies = []
     test_losses = []
+    validation_accuracies = []
+    validation_losses = []
+    keep_prob1_vals = []
+    keep_prob2_vals = []
     over_fit_vals = []
 
     for i in range(iterations):
         ind_train = [randint(0, svhn.train_examples - 1) for _ in range(batch_size)]
+        ind_validation = [randint(0, svhn.validation_examples - 1) for _ in range(batch_size)]
         ind_test = [randint(0, svhn.test_examples - 1) for _ in range(batch_size)]
 
         batch_x = svhn.train_data[ind_train]
         batch_y = svhn.train_labels[ind_train]
 
+        batch_x_validation = svhn.validation_data[ind_validation]
+        batch_y_validation = svhn.validation_labels[ind_validation]
+
         batch_x_test = svhn.test_data[ind_test]
         batch_y_test = svhn.test_labels[ind_test]
 
-        sess.run(optimizer, feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob: 0.8})
+        sess.run(optimizer, feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob1: keep_prob1_value,
+                                       keep_prob2: keep_prob2_value})
 
         if (i + 1) % display_step == 0 or i == 0:
             _accuracy_train, _cost_train = sess.run([accuracy, cost],
-                                                    feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0})
+                                                    feed_dict={X: batch_x, Y: batch_y, keep_prob1: 1.0,
+                                                               keep_prob2: 1.0})
             _accuracy_test, _cost_test = sess.run([accuracy, cost],
-                                                  feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob: 1.0})
-            over_fit_vals.append(_cost_train / _cost_test)
-            print("Step: {0:6d}, Training Accuracy: {1:5f}, Batch Loss: {2:5f}".format(i + 1, _accuracy_train,
-                                                                                       _cost_train))
+                                                  feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob1: 1.0,
+                                                             keep_prob2: 1.0})
+            _accuracy_validation, _cost_validation = sess.run([accuracy, cost],
+                                                              feed_dict={X: batch_x_validation, Y: batch_y_validation,
+                                                                         keep_prob1: 1.0, keep_prob2: 1.0})
+            over_fit_vals.append(_cost_validation / _cost_test)
+            keep_prob1_vals.append(keep_prob1_value)
+            keep_prob2_vals.append(keep_prob2_vals)
+
+            if _cost_validation / _cost_test > 30:
+                keep_prob1_value /= 2
+                keep_prob1_value /= 1.5
+            elif _cost_validation / _cost_test < 3 and np.max([keep_prob1_value, keep_prob2_value]) < 1:
+                keep_prob1_value *= 2
+                keep_prob1_value *= 1.5
+            print("Step: {0:6d}, Train Accuracy: {1:5f}, Batch Loss: {2:5f}".format(i + 1, _accuracy_train,
+                                                                                    _cost_train))
+            print("Step: {0:6d}, Validation Accuracy: {1:5f}, Batch Loss: {2:5f}".format(i + 1, _accuracy_validation,
+                                                                                         _cost_validation))
             print("Step: {0:6d}, Test Accuracy: {1:5f}, Batch Loss: {2:5f}".format(i + 1, _accuracy_test,
                                                                                    _cost_test))
             train_accuracies.append(_accuracy_train)
             train_losses.append(_cost_train)
+            validation_accuracies.append(_accuracy_validation)
+            validation_losses.append(_cost_validation)
             test_accuracies.append(_accuracy_test)
             test_losses.append(_cost_test)
 
-    plt.subplot(311)
+    plt.subplot(411)
     plt.grid(True)
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     test_accuracies_legend, = plt.plot(test_accuracies, label='test')
     train_accuracies_legend, = plt.plot(train_accuracies, label='train')
-    plt.legend([test_accuracies_legend, train_accuracies_legend], ['Train', 'Test'])
+    validation_accuracies_legend, = plt.plot(validation_accuracies, label='validation')
+    plt.legend([test_accuracies_legend, train_accuracies_legend, validation_accuracies_legend],
+               ['Train', 'Test', 'Validation'])
 
-    plt.subplot(312)
+    plt.subplot(412)
     plt.grid(True)
     plt.xlabel('Epoch')
     plt.ylabel('MSE')
     train_losses_legend, = plt.plot(train_losses, label='train_mse')
     test_losses_legend, = plt.plot(test_losses, label='test_mse')
-    plt.legend([train_losses_legend, test_losses_legend], ['Test', 'Train'])
+    validation_losses_legend, = plt.plot(validation_losses, label='validation_mse')
+    plt.legend([train_losses_legend, test_losses_legend, validation_losses_legend], ['Test', 'Train', 'Validation'])
 
-    plt.subplot(313)
+    plt.subplot(413)
     plt.grid(True)
     plt.xlabel('Epoch')
     plt.ylabel(r'$v_{ofc}$')
     plt.plot(over_fit_vals)
 
+    plt.subplot(414)
+    plt.grid(True)
+    plt.xlabel('Epoch')
+    plt.ylabel(r'$kp_{i}$')
+    keep_prob1_legend, = plt.plot(keep_prob1_vals, label='kp1')
+    keep_prob2_legend, = plt.plot(keep_prob2_vals, label='kp2')
+    plt.legend([keep_prob1_legend, keep_prob2_legend], [r'$kp_1$', r'$kp_2$'])
     plt.show()
